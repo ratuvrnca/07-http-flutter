1a0ea284effa6c5c44a228e4ab2dbd9742591c44
ratuvrnca <2031710119@student.polinema.ac.id>
2022-04-28 21:02:01 +0700

    http request

---
+++ android/app/src/main/kotlin/com/example/http_request_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.http_request_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/service/http_service.dart
@@ -0,0 +1,27 @@
+// ignore_for_file: avoid_print
+
+import 'dart:convert';
+import 'dart:io';
+import 'package:http/http.dart' as http;
+import 'package:http_request_flutter/models/movie.dart';
+
+class HttpService {
+  final String apiKey = 'bd53686a3266545baa77aae51daf1d2c';
+  final String baseUrl = 'https://api.themoviedb.org/3/movie/popular?api_key=';
+
+  Future<List?> getPopularMovies() async {
+    final String uri = baseUrl + apiKey;
+
+    http.Response result = await http.get(Uri.parse(uri));
+    if (result.statusCode == HttpStatus.ok) {
+      print("Sukses");
+      final jsonResponse = json.decode(result.body);
+      final moviesMap = jsonResponse['results'];
+      List movies = moviesMap.map((i) => Movie.fromJson(i)).toList();
+      return movies;
+    } else {
+      print("Salah");
+      return null;
+    }
+  }
+}
